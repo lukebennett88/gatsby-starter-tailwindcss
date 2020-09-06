27668fffac26d0444dafb7beb0016fedcbbdf115
Luke Bennett <hello@lukebennett.com.au>
2020-09-06 13:43:46 +1000

    Style focus states

--- src/components/header.js
+++ src/components/header.js
@@ -11,9 +11,12 @@ function Header() {
     <nav className="sticky top-0 z-20 bg-white shadow">
       <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
-          <div className="flex">
-            <Link to="/" className="flex items-center flex-shrink-0">
-              <Logo className="w-auto h-10 text-teal-500 transition duration-150 ease-in-out fill-current hover:text-teal-700" />
+          <div className="flex items-center">
+            <Link
+              to="/"
+              className="flex items-center flex-shrink-0 text-teal-500 transition duration-150 ease-in-out rounded-full hover:text-teal-700 focus:text-teal-700"
+            >
+              <Logo className="h-10 fill-current" />
             </Link>
           </div>
           <div className="hidden space-x-8 sm:ml-6 sm:flex">
@@ -22,8 +25,8 @@ function Header() {
                 key={node.id}
                 to={node.slug}
                 partiallyActive={node.slug !== '/'}
-                activeClassName="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-900 transition duration-150 ease-in-out border-b-2 border-teal-500 focus:outline-none focus:border-teal-700"
-                className="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300"
+                activeClassName="text-gray-900 border-teal-500 focus:border-teal-700"
+                className="inline-flex items-center px-1 pt-1 text-sm font-medium leading-5 text-gray-500 transition duration-150 ease-in-out border-b-2 border-transparent hover:text-gray-700 hover:border-gray-300 focus:shadow-none focus:text-gray-700 focus:border-gray-300"
               >
                 {node.label}
               </Link>
