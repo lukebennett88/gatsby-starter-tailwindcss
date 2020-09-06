244629911c4981bcd894f35ff49b6f9cebb113b9
Luke Bennett <hello@lukebennett.com.au>
2020-09-06 12:18:22 +1000

    Clean up layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,19 +9,13 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import { Header } from './header';
-import { Sidebar } from './sidebar';
 import { Footer } from './footer';
 
 function Layout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen bg-white border-t-4 border-teal-500">
+    <div className="relative flex flex-col min-h-screen font-sans antialiased text-gray-700 bg-white border-t-4 border-teal-500 fill-available">
       <Header />
-      <main className="flex-1 w-full max-w-3xl px-6 mx-auto lg:max-w-7xl">
-        <article className="grid py-12 border-gray-100 lg:grid-cols-3 lg:gap-16">
-          <Sidebar />
-          <div className="col-span-2 space-y-12 lg:py-6">{children}</div>
-        </article>
-      </main>
+      <main className="flex-1">{children}</main>
       <Footer />
     </div>
   );
