e8dd0d6c8ff9770d115e05f54b68b65692f3b2bf
Luke Bennett <hello@lukebennett.com.au>
2020-09-06 12:38:32 +1000

    Use nanoid for IDs

--- src/data/site-navigation.js
+++ src/data/site-navigation.js
@@ -1,3 +1,5 @@
+import { nanoid } from 'nanoid';
+
 import {
   HomeIcon,
   FacebookIcon,
@@ -9,44 +11,42 @@ import {
 
 export const mainNavigation = [
   {
-    id: '2758f798-cb6f-49cf-ab02-17bbf177e006',
+    id: nanoid(),
     label: 'Home',
     slug: '/',
     icon: HomeIcon,
   },
 ];
 
-export const footerNavigation = [
-  { id: '1cb87a4a-d781-4393-81ee-0b1dc604648f', label: 'Home', slug: '/' },
-];
+export const footerNavigation = [{ id: nanoid(), label: 'Home', slug: '/' }];
 
 export const socialLinks = [
   {
-    id: '0f679d69-df8e-4548-94e5-dd5cb1055857',
+    id: nanoid(),
     label: 'Facebook',
     url: 'https://www.facebook.com',
     icon: FacebookIcon,
   },
   {
-    id: 'b224c849-eb7d-43d1-873f-065a19a62dc9',
+    id: nanoid(),
     label: 'Instagram',
     url: 'https://www.instagram.com',
     icon: InstagramIcon,
   },
   {
-    id: '82359cb4-bd0d-47ef-991c-9587d2db249a',
+    id: nanoid(),
     label: 'Twitter',
     url: 'https://www.twitter.com',
     icon: TwitterIcon,
   },
   {
-    id: 'b7c1c8f2-960d-4040-913b-013e4901c1c3',
+    id: nanoid(),
     label: 'GitHub',
     url: 'https://www.github.com',
     icon: GitHubIcon,
   },
   {
-    id: 'c3b7c61f-c16e-4a9a-9b82-1fa31fdfcf0e',
+    id: nanoid(),
     label: 'Dribbble',
     url: 'https://www.dribbble.com',
     icon: DribbbleIcon,
