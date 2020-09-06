30e64d0100c2ae2dbc54b78da5f9f0a2d44753ec
Luke Bennett <hello@lukebennett.com.au>
2020-09-06 13:43:08 +1000

    Add typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 const tailwindUI = require('@tailwindcss/ui');
+const typography = require('@tailwindcss/typography');
 
 // See https://tailwindcss.com/docs/configuration for details
 module.exports = {
@@ -15,5 +16,7 @@ module.exports = {
   plugins: [
     // See https://tailwindui.com/documentation for details
     tailwindUI,
+    // See https://github.com/tailwindlabs/tailwindcss-typography for details
+    typography,
   ],
 };
